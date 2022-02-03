1e4cf293ff99107f19ba9a6c16677b8301644a2f
keijiek <kumatan800@gmail.com>
2022-02-04 00:59:11 +0900

    install eslint and write up eslintrc.json

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
-//import $ from 'jquery';
+// import $ from 'jquery';
 import { add, subtract } from './modules/math';
 
 const item1Price = 400;
 const item2Price = 600;
 const coupon = 300;
 
-const totalPrice = add(item1Price,item2Price);
+const totalPrice = add(item1Price, item2Price);
 const priceAfterApplyCoupon = subtract(totalPrice, coupon);
 
 console.log(Document);
 const body = document.getElementsByTagName('body');
 console.log(body);
 const head1 = document.createElement('h1');
-head1.insertAdjacentHTML('afterbegin','見出し');
+head1.insertAdjacentHTML('afterbegin', priceAfterApplyCoupon);
 console.log(head1);
 
 body[0].appendChild(head1);
--- src/js/modules/math.js
+++ src/js/modules/math.js
@@ -1,7 +1,6 @@
-export function add( number1, number2) {
+export function add(number1, number2) {
   return number1 + number2;
 }
-export function subtract( number1, number2) {
+export function subtract(number1, number2) {
   return number1 - number2;
 }
-
