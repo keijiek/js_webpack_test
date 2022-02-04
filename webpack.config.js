3dcdfaa18573010634ef46d8fd8296cdd8ad1e09
keijiek <kumatan800@gmail.com>
2022-02-05 00:39:31 +0900

    change some webpack.config.js, and code-workspace settings, add typescript packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = (env, argv) => {
+  // const is_development = argv.mode === 'development';
+  // const is_production = argv.mode ==='production';
+  console.log('argv.mode:', argv.mode);
+  const MODE = (argv.mode === 'production') ? 'production' : 'development' ;
+  console.log(MODE);
+
   return {
-    //mode: 'development',
-    mode: 'production',
+    mode: 'development',
+    // mode: 'production',
+
     entry: './src/js/app.js',
+
     output: {
       path: path.resolve(__dirname, 'public'),
       filename: 'js/bundle.js'
