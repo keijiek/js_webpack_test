01634669527c9a42f6b329306626f5dbecf1ae50
keijiek <kumatan800@gmail.com>
2022-02-07 01:06:38 +0900

    html-webpack-plugin is activated.

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Rule: sass-loader css-loader MiniCssExtractPlugin
+// 設定: sass-loader & css-loader & MiniCssExtractPlugin
+const pluginForMiniCssExtract = new MiniCssExtractPlugin({filename: './css/style.css',});
 const ruleForSASS = {
   test: /\.s[ac]ss$/i,
   include: path.resolve(__dirname, 'src/sass'),// module.exports に書かれる output.path を起点としたpath
@@ -12,12 +14,17 @@ const ruleForSASS = {
   ],
 }
 
-// Rule: ts-loader
+// 設定: ts-loader
 const ruleForTypeScript = {
   test: /\.tsx?$/,
   use: 'ts-loader',
   exclude: /node_modules/,
 }
+
+// 設定: html-webpack-plugin
+const pluginForHtmlWebpack = new HtmlWebpackPlugin({
+  template: './src/index.html',// テンプレート
+});
 
 // webpack 設定本編
 module.exports = {
@@ -32,13 +39,13 @@ module.exports = {
   },
   module: {
     rules: [
-      ruleForSASS,
-      ruleForTypeScript,
+      ruleForSASS,// 下記 plugins array に要: mini-css-extract-plugin
+      ruleForTypeScript,// 下記 plugins array に要: html-webpack-plugin
     ],
   },
   plugins: [
-    // css ファイルを出力させるプラグイン, 出力する filename は output.path を起点とした path
-    new MiniCssExtractPlugin({filename: './css/style.css',}),
+    pluginForMiniCssExtract,
+    pluginForHtmlWebpack,
   ],
   resolve: {
     extensions: ['.ts', '...'],// ... は '.js', '.json', '.wasm' のデフォルト値を指す
