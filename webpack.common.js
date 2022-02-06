73317dac6a4199770d5c105652665429b08a3a1a
keijiek <kumatan800@gmail.com>
2022-02-06 19:40:01 +0900

    TypeScript is activated, and compile was successful.

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,47 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// エントリーポイント, ts にするかもしれないので、書き直しやすいよう外側に出しておく
-const ENTRY_POINT_PATH = path.resolve(__dirname, 'src', 'js', 'app.js');
+// a Rule for translation from SASS to CSS
+const ruleForSASS = {
+  test: /\.s[ac]ss$/i,
+  include: path.resolve(__dirname, 'src/sass'),// output.path を起点としたpath
+  use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    'sass-loader',
+  ],
+}
 
+// a Rule for translation from TypeScript to JS
+const ruleForTypeScript = {
+  test: /\.tsx?$/,
+  use: 'ts-loader',
+  exclude: /node_modules/,
+}
+
+// webpack 設定本編
 module.exports = {
+  // 入力の起点
   entry: {
-    'js/bundle': ENTRY_POINT_PATH,
+    'js/bundle': path.resolve(__dirname, 'src', 'ts', 'app.ts'),
   },
+  // 出力するバンドルjs
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].js',
   },
   module: {
     rules: [
-      // a Rule for translation from SASS to CSS
-      {
-        test: /\.s[ac]ss$/i,
-        include: path.resolve(__dirname, 'src/sass'),
-        use: [
-          // 'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      }
+      ruleForSASS,
+      ruleForTypeScript,
     ],
   },
   plugins: [
     // css ファイルを出力させるプラグイン, 出力する filename は output.path を起点とした path
     new MiniCssExtractPlugin({filename: './css/style.css',}),
   ],
+  resolve: {
+    extensions: ['.ts', '...'],// ... は '.js', '.json', '.wasm' のデフォルト値を指す
+    modules: ['node_modules'],// デフォルト値
+  }
 }
